bb0a36358e691f061c5ae15f364fbcc35e44137f
APerson <danielhglus@gmail.com>
2015-04-09 12:20:52 -0400

    parameter magic
    The username can now be specified in the GET request.

--- aadminscore.py
+++ aadminscore.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("aadminscore.html")
+    return render_template("aadminscore.html",
+                           user=request.args.get("user", ""))
 
 if __name__ == "__main__":
     app.run(debug=True)
